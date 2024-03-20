c017fa8847d6b584c416fb4c94da5d9981719bd6
Ayanda Adegbola <ayandakinyambo27@gmail.com>
2024-03-20 10:05:15 +0200

    ai

--- vite.config.ts
+++ vite.config.ts
@@ -13,9 +13,6 @@ export default defineConfig({
         global: true,
         process: true,
       },
-      overrides: {
-        fs: "memfs",
-      },
       protocolImports: true,
     }),
   ],
