9b80f5646db97edbba9427c7305ca6b622b00068
Ayanda Adegbola <ayandakinyambo27@gmail.com>
2024-03-21 12:03:52 +0200

    footer fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,7 @@ import { Separator } from "./ui/separator";
 
 export default function Footer() {
     return (
-        <div
-            className="bg-purple-700 py-5 mb-6">
+        <div className="bg-purple-700 py-5 mb-6">
             <div className="flex flex-col">
                 <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
                     <span className="text-2xl text-white font-bold tracking-tight">
@@ -16,7 +15,10 @@ export default function Footer() {
                 </div>
             </div>
             <Separator className="my-5 bg-gray-400" />
-            <p className="text-gray-200 text-center relative">© {new Date().getFullYear()} Made by Ayanda Kinyambo</p>
+            <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+                <p className="text-gray-200 text-base">© {new Date().getFullYear()} Made by Ayanda Kinyambo</p>
+                <p className="text-gray-200 text-sm md:text-base">Support: support@nomnomnimbus.com</p>
+            </div>
         </div>
     )
 }
